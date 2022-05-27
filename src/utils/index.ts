8a513583d79d77f8e3402988f0af98fc43e0274b
Huzaifa Arshad <01-131182-015@student.bahria.edu.pk>
2022-05-27 11:49:16 +0500

    first function in util updated

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,11 @@
 import {User} from 'types/index';
 
 export const isEmpty = (item: Object) => Object.keys(item).length === 0;
-export const first = (item: Array<User>) => item.find(e => e !== undefined);
+export const first = (item: Array<User>) => {
+  if (item.length > 0) {
+    return item[0];
+  } else {
+    return {};
+  }
+};
 export const getCurrentUserId = store => store.getState().auth.userMeta?.id;
