b6bcc3803e9239321f2d81af98d03a9fa64cdb9c
Huzaifa Arshad <01-131182-015@student.bahria.edu.pk>
2022-06-06 10:03:46 +0500

    ramda usage

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,13 +1,14 @@
 import {User} from 'types/index';
 import Toast from 'react-native-toast-message';
-import {ifElse, isEmpty, always, head} from 'ramda';
+import {ifElse, isEmpty, always, head, pathOr} from 'ramda';
 
 const firstOrEmpty = ifElse(isEmpty, always([]), head);
 
 export const first = (item: Array<User>) => {
   return firstOrEmpty(item);
 };
-export const getCurrentUserId = store => store.getState().auth.userMeta?.id;
+export const getCurrentUserId = store =>
+  pathOr(null, ['auth', 'userMeta', 'id'], store.getState());
 export const addToast = (message: string, type: string) => {
   Toast.show({
     type: type,
