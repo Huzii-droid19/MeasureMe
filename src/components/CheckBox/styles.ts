ff3561cc90ec7023c22bab15413d5fed17ad5fea
Huzaifa Arshad <01-131182-015@student.bahria.edu.pk>
2022-06-11 12:04:20 +0500

    Changed UI for Edit Screen

---
+++ src/components/CheckBox/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+import {CheckBox, Layout, Text} from '@ui-kitten/components';
+
+export const StyledCheckBox = styled(CheckBox)({
+  alignSelf: 'flex-start',
+  marginTop: 10,
+  marginBottom: 20,
+  marginHorizontal: 20,
+});
+export const CheckBoxLabel = styled(Text)({
+  fontSize: 16,
+  fontWeight: 'bold',
+});
--- src/components/MeetupButton/index.tsx
+++ src/components/MeetupButton/index.tsx
@@ -1,39 +1,44 @@
 import {useTheme} from '@ui-kitten/components';
 import React from 'react';
-import {AddMeetupButton, IconContainer, MeetLabel, StyledIcon} from './styles';
-
+import {
+  AddMeetupButton,
+  IconContainer,
+  MeetLabel,
+  StyledIcon,
+  StyledImageIcon,
+} from './styles';
 interface Props {
   isMeetupAdded: boolean;
   setIsMeetUpAdded: (isMeetupAdded: boolean) => void;
 }
+const icon =
+  'https://fonts.gstatic.com/s/i/productlogos/meet_2020q4/v6/web-512dp/logo_meet_2020q4_color_2x_web_512dp.png';
 
 const MeetupButton = ({isMeetupAdded, setIsMeetUpAdded}: Props) => {
-  const [meetState, setMeetState] = React.useState({
-    iconName: 'video-off-outline',
-    meetUpText: 'Add video conference',
-  });
-
+  const [meetUpText, setMeetUpText] = React.useState(
+    isMeetupAdded ? 'Video conference added' : 'Add Video Conference',
+  );
   const theme = useTheme();
-  const handleAddMeetUp = () => {
-    setMeetState({
-      iconName: 'video-outline',
-      meetUpText: 'Video conference added',
-    });
-    setIsMeetUpAdded(true);
+  const handleMeetup = () => {
+    setIsMeetUpAdded(!isMeetupAdded);
+    setMeetUpText(
+      isMeetupAdded ? 'Add Video Conference' : 'Video conference added',
+    );
   };
-  const handleRemoveMeetUp = () => {
-    setMeetState({
-      iconName: 'video-off-outline',
-      meetUpText: 'Add video conference',
-    });
-    setIsMeetUpAdded(false);
-  };
+
+  const renderIcon = () =>
+    isMeetupAdded ? (
+      <StyledImageIcon source={{uri: icon}} />
+    ) : (
+      <StyledIcon name="video-off-outline" fill={theme['text-hint-color']} />
+    );
+
   return (
-    <AddMeetupButton onPress={handleAddMeetUp} disabled={isMeetupAdded}>
-      <StyledIcon name={meetState.iconName} fill={theme['text-hint-color']} />
-      <MeetLabel>{meetState.meetUpText}</MeetLabel>
+    <AddMeetupButton onPress={handleMeetup} disabled={isMeetupAdded}>
+      {renderIcon()}
+      <MeetLabel>{meetUpText}</MeetLabel>
       {isMeetupAdded && (
-        <IconContainer onPress={handleRemoveMeetUp}>
+        <IconContainer onPress={handleMeetup}>
           <StyledIcon name="close-outline" fill={theme['text-hint-color']} />
         </IconContainer>
       )}
